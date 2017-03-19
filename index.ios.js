da1c9b2ae7dca616dfdf416014480dcf3a943951
June Ha <juneha@dhcp-206-87-208-6.ubcsecure.wireless.ubc.ca>
2017-03-18 22:25:19 -0700

    create new page

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,9 @@ import {
   ListView,
   NavigatorIOS,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  TextInput,
 } from 'react-native';
 
 
@@ -18,53 +20,64 @@ var styles = StyleSheet.create({
     fontSize: 30,
     margin: 80,
     fontSize: 16,
+    flex: 1,
   },
   container: {
     flex: 1,
   }
 });
 
-class MainScreen extends Component {
-  render() {
-    return <Text style={styles.text}>grfiti (Again)</Text>;
-  }
-}
 
 class grfiti extends Component {
+  createNewSegueClick() {
+    this.refs.nav.push({
+      component: CreateScreen,
+      title: "Create New",
+    })
+
+  }
   render() {
     return (
       <NavigatorIOS
-        style={styles.container}
-        initialRoute={{
+        ref = 'nav'
+        style = {styles.container}
+        initialRoute = {{
           title: 'Feed',
-          rightButtonTitle: '+', 
-          component: ListViewScreen,
-        }}/>
-    );
-  }
-}
-
-class ListViewScreen extends Component {
-  constructor(props) {
-    super(props);
-
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-    };
-  }
-  render() {
-    return (
-      <ListView
-        style={styles.container}
-        dataSource={this.state.dataSource}
-        renderRow={(data) => <View><Text>{data}</Text></View>}
+          rightButtonTitle: '+', //image for plus
+          onRightButtonPress: () => this.createNewSegueClick(),
+          component: MainScreen,
+        }}
       />
     );
   }
 }
 
+class MainScreen extends Component {
+  render() {
+    return <Text style={styles.text}>Will change to ListView</Text>;
+  }
+}
 
+class CreateScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Post a Message!',
+    };
+  }
+
+  render() {
+    return (
+      <View>
+         <TextInput 
+         style={{borderWidth: 50, borderColor: 'white'}}
+         onChangeText={(text) => this.setState({text})} 
+         value={this.state.text} 
+         />
+     </View>
+    );
+  }
+}
 
 
 AppRegistry.registerComponent('grfiti', function() { return grfiti });
