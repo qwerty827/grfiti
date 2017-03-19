f23fef65728fd024364b5adb7b83975950c135ea
June Ha <juneha@dhcp-206-87-208-6.ubcsecure.wireless.ubc.ca>
2017-03-18 20:26:36 -0700

    ListView start

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  ListView,
   NavigatorIOS,
   Text,
   View
@@ -15,10 +16,11 @@ var styles = StyleSheet.create({
     color: 'black',
     backgroundColor: 'white',
     fontSize: 30,
-    margin: 80
+    margin: 80,
+    fontSize: 16,
   },
   container: {
-    flex: 1
+    flex: 1,
   }
 });
 
@@ -36,11 +38,33 @@ class grfiti extends Component {
         initialRoute={{
           title: 'Feed',
           rightButtonTitle: '+', 
-          component: MainScreen,
+          component: ListViewScreen,
         }}/>
     );
   }
 }
+
+class ListViewScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(data) => <View><Text>{data}</Text></View>}
+      />
+    );
+  }
+}
+
+
 
 
 AppRegistry.registerComponent('grfiti', function() { return grfiti });
